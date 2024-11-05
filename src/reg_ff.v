e4db4273d48ded1b5c71cdbf79b0ee342a3ec1c0
Devesh Bhaskaran <devesh6742@gmail.com>
2024-11-05 21:30:19 +0530

    changed reg file

--- src/reg_ff.v
+++ src/reg_ff.v
@@ -1,20 +1,20 @@
 module reg_ff #(parameter DATA_WIDTH = 8) (
     input       clk,
-    input       wr_en, rs1_en, rs2_en,
-    input       [2:0] rd_addr1, rd_addr2, wr_addr,
-    input       [DATA_WIDTH-1:0] wr_data,
-    output      [DATA_WIDTH-1:0] rd_data1, rd_data2
+    input       wr_en,
+    input       [2:0] rs1, rs2, rd,
+    input       [DATA_WIDTH-1:0] din,
+    output      [DATA_WIDTH-1:0] r1, r2
 );
 
 reg [DATA_WIDTH-1:0] reg_file_arr [0:2];
 
 // register file write logic (synchronous)
 always @(posedge clk) begin
-    if (wr_en) reg_file_arr[wr_addr] <= wr_data;
+    if (wr_en) reg_file_arr[rd] <= din;
 end
 
 // register file read logic (combinational)
-assign rd_data1 = ( rs1_en != 0 & rd_addr1 != 0) reg_file_arr[rd_addr1] : 0;
-assign rd_data2 = ( rs2_en != 0 & rd_addr2 != 0) ? reg_file_arr[rd_addr2] : 0;
+assign r1 = ( rs1 != 0 ) reg_file_arr[rs1] : 0;
+assign r2 = ( rs2 != 0 ) ? reg_file_arr[rs2] : 0;
 
 endmodule
