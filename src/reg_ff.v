d20f4131c2e555be38c4fcdb106e32756e00b290
Devesh Bhaskaran <devesh6742@gmail.com>
2024-11-05 10:48:14 +0530

    changed address width and consition for zero register

--- src/reg_ff.v
+++ src/reg_ff.v
@@ -1,12 +1,12 @@
 module reg_ff #(parameter DATA_WIDTH = 8) (
     input       clk,
     input       wr_en, rs1_en, rs2_en,
-    input       [5:0] rd_addr1, rd_addr2, wr_addr,
+    input       [2:0] rd_addr1, rd_addr2, wr_addr,
     input       [DATA_WIDTH-1:0] wr_data,
     output      [DATA_WIDTH-1:0] rd_data1, rd_data2
 );
 
-reg [DATA_WIDTH-1:0] reg_file_arr [0:3];
+reg [DATA_WIDTH-1:0] reg_file_arr [0:2];
 
 // register file write logic (synchronous)
 always @(posedge clk) begin
@@ -14,7 +14,7 @@ always @(posedge clk) begin
 end
 
 // register file read logic (combinational)
-assign rd_data1 = ( rs1_en != 0 ) ? reg_file_arr[rd_addr1] : 0;
-assign rd_data2 = ( rs2_en != 0 ) ? reg_file_arr[rd_addr2] : 0;
+assign rd_data1 = ( rs1_en != 0 & rd_addr1 != 0) reg_file_arr[rd_addr1] : 0;
+assign rd_data2 = ( rs2_en != 0 & rd_addr2 != 0) ? reg_file_arr[rd_addr2] : 0;
 
 endmodule
