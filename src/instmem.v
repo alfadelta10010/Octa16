02d6e6a541917da62e14c19ea5d127b6ce523061
Devesh Bhaskaran <devesh6742@gmail.com>
2024-11-10 20:45:20 +0530

    Added Inst mem and data mem

---
+++ src/instmem.v
@@ -0,0 +1,15 @@
+module instmem #(parameter SIZE = 12) (clk, memW, addr, dataW, dataR);
+    input wire clk;
+    input wire memW;
+    input wire [SIZE-1:0] addr;
+    input wire [7:0] dataW;
+    output wire [7:0] dataR;
+    reg [7:0] mem [(SIZE-1):0];
+
+    always @(posedge clk) begin
+        if (memW) begin
+            mem[addr] <= dataW;
+        end
+    end
+    assign dataR = mem[addr];
+endmodule
