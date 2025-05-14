549cce3d668f100dbf4a166f62295b42088864c8
Abhiram Dasika <alfadelta10010@gmail.com>
2025-05-14 22:21:11 +0530

    made it 8 bit lol

--- src/regfile.sv
+++ src/regfile.sv
@@ -1,17 +1,17 @@
 module RegisterFile(clk, wen, rd, rs1, rs2, r1, r2, din);
   input logic clk, wen;
-  input logic [4:0] rd;
-  input logic [4:0] rs1;
-  input logic [4:0] rs2;
-  input logic [31:0] din;
-  output logic [31:0] r1;
-  output logic [31:0] r2;
+  input logic [2:0] rd;
+  input logic [2:0] rs1;
+  input logic [2:0] rs2;
+  input logic [7:0] din;
+  output logic [7:0] r1;
+  output logic [7:0] r2;
 
-  logic [31:0] regs [0:31];
+  logic [7:0] regs [0:7];
 
   initial begin
-    for (int i = 0; i < 32; i++)
-      regs[i] <= 32'b0;
+    for (int i = 0; i < 8; i++)
+      regs[i] <= 8'b0;
   end
   always @(posedge clk)
     if (wen) 
